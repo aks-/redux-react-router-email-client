e5de9a6d29ac2fa93746a8b772279263b4259017
aks- <coderatlabs@gmail.com>
2015-12-24 23:17:52 +0530

    use combineReducers from redux

--- reducers/index.js
+++ reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import {
   Map,
   List,
@@ -5,32 +6,15 @@ import {
 } from 'immutable';
 import generateRandomString from '../lib/generateRandomString';
 import { emails } from './emails';
-import { selectBox } from './selectBox';
+import { boxSelector } from './selectBox';
 import { emailSelector } from './emailSelector';
 import { unread } from './unread';
 import { userInfo } from './userInfo';
 
-export const emailApp = (state = {}, action) => {
-  return {
-    selectedEmailIndex: emailSelector(
-      state.selectedEmailIndex,
-      action
-    ),
-    userInfo: userInfo(
-      state.userInfo,
-      action
-    ),
-    selectedBox: selectBox(
-      state.selectedBox,
-      action
-    ),
-    emails: emails(
-      state.emails,
-      action
-    ),
-    unread: unread(
-      state.unread,
-      action
-    )
-  };
-};
+export const emailApp = combineReducers({
+  selectedEmailIndex: emailSelector,
+  selectedBox: boxSelector,
+  userInfo,
+  emails,
+  unread
+});
--- reducers/selectBox.js
+++ reducers/selectBox.js
@@ -1,6 +1,6 @@
 const SELECT_BOX = 'SELECT_BOX';
 
-export const selectBox = (state = 'inbox', action) => {
+export const boxSelector = (state = 'inbox', action) => {
   switch (action.type) {
     case SELECT_BOX:
       return action.box;
