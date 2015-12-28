597367b081efb72f57e1895068788729e0700e42
aks- <coderatlabs@gmail.com>
2015-12-28 16:03:03 +0530

    seperate client code to its directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 module.exports = {
+  context: __dirname + "/client",
   entry: './main.js',
   output: {
     path: './',
-    filename: 'dist/index.js'
+    filename: '/dist/index.js'
   },
   devServer: {
+    contentBase: "./client",
     inline: true,
     port: 3001
   },
   module: {
     loaders: [{
       test: /\.js$/,
-      exclude: /node_modules/,
+      exclude: /(node_modules|server)/,
       loader: 'babel',
       query: {
         presets: ['es2015', 'react'],
