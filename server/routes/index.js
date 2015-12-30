c149ea260882b61dbe91d117a2bc6968b03b6d9e
aks- <coderatlabs@gmail.com>
2015-12-31 02:44:38 +0530

    change api from get to post

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,53 @@
+import * as db from '../lib/db';
+
+const design = 'emails';
+
 module.exports = (router) => { 
-  router.get('/sent-items', function(req, res) {
-    const email = req.body.email; 
+  router.post('/sent', (req, res) => {
+    const view = 'byFromEmail';
+    const key = req.body.email;
+
+    getItems(design, view, {
+      key: [key]
+    })
+    .then(items => {
+      res.status(200).json(items)
+    })
   });
 
-  router.get('/inbox', function(req, res) {
-    const email = req.body.email;
+  router.post('/inbox', (req, res) => {
+    const view = 'byToEmail';
+    const key = req.body.email;
+
+    getItems(design, view, {
+      key
+    })
+    .then(items => {
+      res.status(200).json(items);
+    });
   });
 
-  router.get('/unread', function(req, res) {
-    const email = req.body.email;
+  router.post('/unread', (req, res) => {
+    var view = 'byToEmail';
+    const key = req.body.email;
+
+    getItems(design, view, {
+      key
+    })
+    .then(items => {
+      var unread = items.length;
+      res.status(200).json(unread);
+    })
   });
 }; 
+
+const getItems = (design, view, keyObj) => {
+  return db.findBy(design, view, keyObj)
+  .then(body => {
+    var rows = body.rows;
+    var items = rows.map(row => {
+      return row.value;
+    });
+    return items;
+  });
+};
