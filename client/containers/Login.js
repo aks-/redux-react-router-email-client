00330bd18bea96811da3e1cb4cb75139ed5246d9
aks- <coderatlabs@gmail.com>
2016-01-02 13:40:35 +0530

    style login page

--- client/containers/Login.js
+++ client/containers/Login.js
@@ -16,8 +16,19 @@ const Login = ({
   onLogIn
 }) => {
   let selectedEmail;
+  const divStyle = {
+    width: 100,
+    height: 100,
+    backgroundColor: '#e3e3e',
+    position: 'absolute',
+    top:0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto'
+  };
   return (
-    <div>
+    <div style={divStyle}>
       <select ref={node => {
         selectedEmail=node
       }}>
